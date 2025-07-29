116c0674d9685983b1f56bac517a7708905e23a7
Trong Thang Pham <phamtrongthang123@gmail.com>
2025-07-28 20:07:29 -0500

    attention score is all None. So now we need to do something else.

---
+++ analyze_attention/medgemma/inference.py
@@ -0,0 +1,77 @@
+import snoop
+
+snoop.install(out="debug_output.txt")
+
+
+def main():
+    # pip install accelerate
+    import torch
+    from PIL import Image
+    from transformers import Gemma3ForConditionalGeneration, Gemma3Processor
+
+    model_id = "google/medgemma-4b-it"
+
+    model = Gemma3ForConditionalGeneration.from_pretrained(
+        model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    processor = Gemma3Processor.from_pretrained(model_id)
+
+    # Load your list of images
+    import json
+    import os
+
+    with open("./jpg_files_list.json") as f:
+        a = json.load(f)
+        image_list = a["file_paths"]
+        root_dir = a["root"]
+
+    image_path = image_list[0]
+    image = Image.open(os.path.join(root_dir, image_path))
+
+    messages = [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": "You are an expert radiologist."}],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe the findings of the chest x-ray in a paragraph with short sentences.",
+                },
+                {"type": "image", "image": image},
+            ],
+        },
+    ]
+
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device, dtype=torch.bfloat16)
+
+    input_len = inputs["input_ids"].shape[-1]
+
+    with torch.inference_mode():
+        generation = model.generate(
+            **inputs,
+            max_new_tokens=200,
+            do_sample=False,
+            output_attentions=True,
+            return_dict_in_generate=True,
+        )
+        attention_scores = generation.attentions
+        generation_ = generation.sequences[0][input_len:]
+
+    decoded = processor.decode(generation_, skip_special_tokens=True)
+    print(decoded)
+    print(attention_scores)
+
+
+if __name__ == "__main__":
+    main()
---
+++ analyze_attention/medgemma/inference_withsnoop.py
@@ -0,0 +1,70 @@
+import snoop
+
+snoop.install(out="debug_output.txt")
+
+
+@snoop
+def main():
+    # pip install accelerate
+    import torch
+    from PIL import Image
+    from transformers import AutoModelForImageTextToText, AutoProcessor
+
+    model_id = "google/medgemma-4b-it"
+
+    model = AutoModelForImageTextToText.from_pretrained(
+        model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    # Load your list of images
+    import json
+    import os
+
+    with open("./jpg_files_list.json") as f:
+        a = json.load(f)
+        image_list = a["file_paths"]
+        root_dir = a["root"]
+
+    image_path = image_list[0]
+    image = Image.open(os.path.join(root_dir, image_path))
+
+    messages = [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": "You are an expert radiologist."}],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe the findings of the chest x-ray in a paragraph with short sentences.",
+                },
+                {"type": "image", "image": image},
+            ],
+        },
+    ]
+
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device, dtype=torch.bfloat16)
+
+    input_len = inputs["input_ids"].shape[-1]
+
+    with torch.inference_mode():
+        generation = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+        generation = generation[0][input_len:]
+
+    decoded = processor.decode(generation, skip_special_tokens=True)
+    print(decoded)
+
+
+if __name__ == "__main__":
+    main()
